417a2d3fb664ac2d7ca458dfbf45c714f6630057
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2021-07-09 23:28:14 +0530

    Add files via upload

---
+++ Zeros_matix.java
@@ -0,0 +1,54 @@
+package code_camp;
+import java.util.*;
+public class Zeros {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter row and col sizes: ");
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		System.out.println("Enter "+n+" X "+m+" Elements: ");
+		int mat[][] = new int[n][m];
+		int row[] = new int[n];
+		int col[] = new int[m];
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j < m; j++)
+			{
+				mat[i][j] = sc.nextInt();
+				if(mat[i][j] == 0 )
+				{
+					row[i] = 1;
+					col[j] = 1;
+				}
+			}
+		}
+		for (int i = 0; i<n; i++)
+		{
+			if(row[i] == 1)
+			{
+				for(int j = 0; j<m; j++)
+				{
+					mat[i][j] = 0;
+				}
+			}
+		}
+		for (int j = 0; j<m; j++)
+		{
+			if(col[j] == 1)
+			{
+				for(int i = 0; i<n; i++)
+				{
+					mat[i][j] = 0;
+				}
+			}
+		}
+		for(int i = 0; i<n; i++)
+		{
+			for(int j = 0; j<m; j++)
+			{
+				System.out.print(mat[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
